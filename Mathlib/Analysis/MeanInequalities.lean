ff02a1627c7054bcba17125aa6e06ba4062a9ec0
Batixx <s59fpern@uni-bonn.de>
2026-06-23 03:50:56 +0200

    .

--- Mathlib/Analysis/MeanInequalities.lean
+++ Mathlib/Analysis/MeanInequalities.lean
@@ -1105,8 +1105,6 @@ namespace ENNReal
 
 variable (f g : ι → ℝ≥0∞) {p q : ℝ}
 
--- TODO: fix the non-terminal simp on the last line
-set_option linter.flexible false in
 /-- **Hölder inequality**: the scalar product of two functions is bounded by the product of their
 `L^p` and `L^q` norms when `p` and `q` are conjugate exponents. Version for sums over finite sets,
 with `ℝ≥0∞`-valued functions. -/
@@ -1123,12 +1121,15 @@ theorem inner_le_Lp_mul_Lq (hpq : p.HolderConjugate q) :
   replace H' : (∀ i ∈ s, f i ≠ ⊤) ∧ ∀ i ∈ s, g i ≠ ⊤ := by
     simpa [ENNReal.rpow_eq_top_iff, asymm hpq.pos, asymm hpq.symm.pos, hpq.pos, hpq.symm.pos,
       ENNReal.sum_eq_top, not_or] using H'
-  have := ENNReal.coe_le_coe.2 (@NNReal.inner_le_Lp_mul_Lq _ s (fun i => ENNReal.toNNReal (f i))
-    (fun i => ENNReal.toNNReal (g i)) _ _ hpq)
-  simp [ENNReal.coe_rpow_of_nonneg, hpq.pos.le, hpq.symm.pos.le] at this
-  convert! this using 1 <;> [skip; congr 2] <;> [skip; skip; simp; skip; simp] <;>
-    · refine Finset.sum_congr rfl fun i hi => ?_
-      simp [H'.1 i hi, H'.2 i hi, -WithZero.coe_mul]
+  convert! ENNReal.coe_le_coe.2 (@NNReal.inner_le_Lp_mul_Lq _ s (fun i => ENNReal.toNNReal (f i))
+    (fun i => ENNReal.toNNReal (g i)) _ _ hpq) <;> simp only [one_div, ofNNReal_finsetSum, coe_mul]
+  · congr! with x hx
+    · exact (coe_toNNReal (H'.1 x hx)).symm
+    · exact (coe_toNNReal (H'.2 x hx)).symm
+  · simp only [inv_nonneg, hpq.pos.le, coe_rpow_of_nonneg, ofNNReal_finsetSum, hpq.symm.pos.le]
+    congr! with x hx x hx
+    · exact (coe_toNNReal (H'.1 x hx)).symm
+    · exact (coe_toNNReal (H'.2 x hx)).symm
 
 /-- **Weighted Hölder inequality**. -/
 lemma inner_le_weight_mul_Lp_of_nonneg (s : Finset ι) {p : ℝ} (hp : 1 ≤ p) (w f : ι → ℝ≥0∞) :
